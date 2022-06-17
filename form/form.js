1b4dd4c6bd5852fb14f2f1500e4f8ac56f68cb5a
Jasmin1002 <jasmineolofin@gmail.com>
2022-06-17 09:35:57 +0100

    update validation for email input

--- form/form.js
+++ form/form.js
@@ -16,41 +16,43 @@ function checkInput(el, message) {
     if (el.value.trim() === '') {
         errorFlag.innerText = `${context.placeholder} ${message}`
         errorFlag.style.opacity = 1;
-        // context.style.borderColor = 'red';
-        context.addEventListener('click', event => {
-            // event.target.blur();
-        })
         context.classList.add('error');
     } else {
         context.classList.remove('error');
         errorFlag.style.opacity = 0;
         isFilled = true;
     }
-
-    return isFilled;
 }
 
-function validateEmail(el) {
-    // const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-    // const controls = el.parentElement.children;
-    // const errorFlag = controls[1];
-    // const context = controls[0];
-
-    // if (el.value.trim().match(validRegex)) {
-    //     context.classList.add('error');
-    //     errorFlag.innerText = `${context.placeholder} contains invalid token`;
-    //     context.style.color = 'hsl(0, 100%, 74%)';
-    // }
-}
+function validateEmail(elementValue){      
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(elementValue); 
+  } 
 
 function validateInput() {
     checkInput(firstnameInput, 'cannot be empty');
     checkInput(lastnameInput, 'cannot be empty');
-    checkInput(emailInput, 'cannot be empty');
-    validateEmail(emailInput);
     checkInput(passwordInput, 'cannot be empty');
-    // return;
+    if (emailInput.value.trim() === '') {
+        checkInput(emailInput, 'cannot be empty');
+    } else if (validateEmail(emailInput.value) === false) {
+        const controls = emailInput.parentElement.children;
+        const errorFlag = controls[1];
+        const context = controls[0];
+
+        context.style.color = 'hsl(0, 100%, 74%)';
+        errorFlag.innerText = "Looks like this is not an email"
+        errorFlag.style.opacity = 1;
+        context.classList.add('error');
+    } else if (validateEmail(emailInput.value)) {
+        const controls = emailInput.parentElement.children;
+        const errorFlag = controls[1];
+        const context = controls[0];
+
+        context.classList.remove('error');
+        errorFlag.style.opacity = 0;
+    }
+    return;
 }
 
 form.addEventListener('submit', event => {
